fac70dfc5c04d67cf058a87f4ec3484d85ed0651
Aridane <ajsarri@gmail.com>
2012-12-01 20:03:20 +0000

    Añadidas cabeceras de algunas funciones a codegenutils.h

--- p4_semantico/src/codegenutils.h
+++ p4_semantico/src/codegenutils.h
@@ -1,2 +1,18 @@
 /*This file intends to keep the defeinitions of the procedures and functions that will used in
 the code generation. Also the global variables and data structures*/
+
+/*gc creates the code line that the code generation part passes*/
+
+gc(char *str);
+
+/*Registers a new label and returns the identifier*/
+int ne();
+
+/*Returns an available register*/
+int assig_regs();
+
+/*Frees all the registers*/
+int lib_regs();
+
+/*Frees the i-th register*/
+int lib_reg(int i);

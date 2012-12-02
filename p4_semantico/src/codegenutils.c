3a8574138a37b3bb286987695d22ecfdd6b6fdd7
Aridane <ajsarri@gmail.com>
2012-12-02 19:02:52 +0000

    El de antes era de pega

--- p4_semantico/src/codegenutils.c
+++ p4_semantico/src/codegenutils.c
@@ -4,7 +4,7 @@
 needed for the code generation part*/
 
 
-int Registers = {0,0,0,0,0,0,0,0}
+int Registers[8] = {0,0,0,0,0,0,0,0};
 int NRegs = 8;
 int NEtiquetas = 0;
 /**************************************************************************/
@@ -27,8 +27,8 @@ int assig_regs(int type)
 {
     int i=0;
 
-/*Buscar un Registro*/
-    if (type == 0) && (NRegs>0)
+	/*Buscar un Registro*/
+    if ((type == 0) && (NRegs>0))
     {
         for (i=0;i<8;i++)
         {
@@ -40,7 +40,7 @@ int assig_regs(int type)
             }
         }
     }
-    elsif (type == 1) && (NRegs>1)
+    else if ((type == 1) && (NRegs>1))
     {
         for (i=0;i<8;i=i+2) //0, 2, 4, 6, -> 0 1 2 3
         {
@@ -48,7 +48,7 @@ int assig_regs(int type)
             {
                 Registers[i] = 1;
 
-Registers[i+1] = 1;
+				Registers[i+1] = 1;
                 NRegs = NRegs - 2;
                 return (i/2);    //Devuelves el identificador no la posición en el vector
             }
@@ -83,26 +83,23 @@ int free_reg(int i, int type)
 {
     if (i < 0) return -1;
     if (i > 7) return -2;
-    if (type != 0 && type != 1) return -3;
+    if ((type != 0) && (type != 1)) return -3;
 
-    if (type == 0 && (NRegs<8))
-
-{
+    if ((type == 0 && (NRegs<8))
+	{
         Registers[i]=0;
-
-NRegs--;
+		NRegs--;
     }
 
-elsif (type == 1 && (NRegs<7))
+	else if ((type == 1 && (NRegs<7))
+	{
 
-{
+    	if ((i % 2) == 1)) return -4;
 
-    if ((i % 2) == 1) return -4;
-
-    Registers[i]=0;
+    	Registers[i]=0;
         Registers[i+1]=0;
 
-NRegs = NRegs-2;
+		NRegs = NRegs-2;
     }
     return 0;
 }
